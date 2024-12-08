c6dd3979ad4e8862d1f823fa6d7daa9aede7822a
Xamacardoso <xcardoso1997@gmail.com>
2024-12-08 00:48:00 -0300

    feat: added image upload and multer integration

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,7 +1,7 @@
 // This file is responsible for handling all the REQUESTS related to posts.
 // It's an intermediaary between the routes and the models
 import { getAllPosts, createPost } from "../models/postModel.js";
-
+import fs from "fs";
 export async function listAllPosts(req, res){
     const posts = await getAllPosts();
     // Server sends a OK status code as response
@@ -14,6 +14,31 @@ export async function insertNewPost(req, res){
 
     try {
         const createdPost = await createPost(newPost);
+        res.status(201).json(createdPost);
+
+    } catch (error) {
+        console.error("Error creating post: ", error.message);
+        res.status(500).json({ error: "Error creating post" });
+    }
+}
+
+export async function uploadImage(req, res){
+    const newPost = {
+        description: "",
+        imgUrl: req.file.originalname,
+        alt: "",
+    };
+
+    try {
+        const createdPost = await createPost(newPost);
+
+        // Rename the uploaded file with the new name based on the inserted post ID
+        const idInString = createdPost.insertedId.toString();
+        const updatedImagePath = `uploads/${idInString}.png`;
+        console.log(updatedImagePath);
+
+        // renames the file from the temporary path to the new path
+        fs.renameSync(req.file.path, updatedImagePath);
         res.status(201).json(createdPost);
 
     } catch (error) {
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,7 +1,28 @@
 // This file is responsible for handling the routes and disposing them
 
 import express from "express";
-import { insertNewPost, listAllPosts } from "../controllers/postsController.js";
+import { insertNewPost, listAllPosts, uploadImage } from "../controllers/postsController.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    // Sets the destination directory for uploaded files
+    destination: function (req, file, cb) {
+        // Calls the callback with null (no error) and the directory "uploads/"
+        cb(null, "uploads/");
+    },
+    // Sets the filename for the uploaded files in the destination directory
+    filename: function (req, file, cb) {
+        // Calls the callback with null (no error) and the original name of the file
+        cb(null, file.originalname);
+    }
+});
+
+
+// on Windows
+const upload = multer({dest: "./uploads", storage}); // Multer instance with destiny folder
+
+// Linux or Mac
+// const upload = multer({dest: "./uploads"});
 
 // Function that handles the routes.
 const routes = (app) => {
@@ -10,6 +31,7 @@ const routes = (app) => {
     // Defines the route for getting a resource (GET Method) and what it executes and returns
     app.get("/posts",listAllPosts);
     app.post("/posts", insertNewPost);
+    app.post("/upload", upload.single("img"), uploadImage);
 }
 
 export default routes;
